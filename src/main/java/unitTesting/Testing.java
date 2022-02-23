c6fcd3305ecff7a7414175eb3cdbd61eb91c94a1
Laura Oliveira <lauranunest@hotmail.com>
2022-02-23 22:00:37 +0000

    atualizando

--- src/main/java/unitTesting/Testing.java
+++ src/main/java/unitTesting/Testing.java
@@ -1,7 +1,21 @@
 package unitTesting;
 
 public class Testing {
-    public static void display(String param){
-        System.out.println("You parameter is:" + param);
-    }
+
+        public double subtrair(double x, double y) {
+            return x - y;
+        }
+
+        public double somar(double x, double y) {
+            return x + y;
+        }
+
+        public double multiplicar(double x, double y) {
+            return x * y;
+        }
+
+        public double dividir(double x, double y) {
+            return x / y;
+        }
+
 }
